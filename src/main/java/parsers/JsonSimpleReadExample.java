89957c55382780f8d0bc08cf0c3f008cbd90e173
Ana Budai <ana.budai@gamesys.co.uk>
2018-09-04 23:00:48 +0100

    test

--- src/main/java/parsers/JsonSimpleReadExample.java
+++ src/main/java/parsers/JsonSimpleReadExample.java
@@ -3,6 +3,7 @@ package parsers;
 public class JsonSimpleReadExample {
 
 	public static void main(String[] parameters) {
+		String test = "a";
 		String json = "{\"name\":\"Robert\", surname\":\"Buican\"}";
 	}
 
